3f370179555e65dac3d09e11d11635bbd9633de7
lenpel <26303224+lenpel@users.noreply.github.com>
2019-11-29 12:35:51 +0100

    Updates

---
+++ precache-manifest.f15fae42e910a282bafeb42c7a230824.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02fce5c478701691821fe38b1a0e4a87",
+    "url": "/paint-app/index.html"
+  },
+  {
+    "revision": "b4a9851f84b4a367626a",
+    "url": "/paint-app/static/css/main.fc583f21.chunk.css"
+  },
+  {
+    "revision": "8c5810439631f7b60138",
+    "url": "/paint-app/static/js/2.97fa161c.chunk.js"
+  },
+  {
+    "revision": "b4a9851f84b4a367626a",
+    "url": "/paint-app/static/js/main.8502cc15.chunk.js"
+  },
+  {
+    "revision": "8dabd5d23f539ffeb402",
+    "url": "/paint-app/static/js/runtime-main.d64062cc.js"
+  }
+]);
